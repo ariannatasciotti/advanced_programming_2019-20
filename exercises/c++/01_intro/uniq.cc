028f9e0e458d1a0ec807785b86b7d42edc0b305d
Arianna Tasciotti <aria.tasc7@gmail.com>
2019-10-17 19:21:48 +0200

    solved_01_intro_exercises

--- exercises/c++/01_intro/uniq.cc
+++ exercises/c++/01_intro/uniq.cc
@@ -6,19 +6,18 @@ int main() {
   std::string s1;
   std::string s2;
   int i{1};
-    
-  std::getline(std::cin,s2);
   
-  std::cout << i << " " << s2 << "\n";
+  std::getline(std::cin,s1);
   
-  while(std::getline(std::cin,s1)) { 
+  while(std::getline(std::cin,s2)) { 
     if(s1== s2){
       i ++; 
-      std::cout << i << " " << s2 << "\n";
       } 
       else {
-		std::cout << 1 << " " << s1 << "\n";
+		std::cout << "  " << i << " " << s1 << "\n";
+		i = 1;
       }
+      s1 = s2;
   }
  
   return 0;
